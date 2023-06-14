9b1ff5f4e07c8e16c4c989b8fc0261f7214a6fc1
hunainmurtaza <hunainmurtaza112@gmail.com>
2023-06-14 21:53:35 +0500

    Student Module

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My App</title>
+    <!-- Include your CSS and JS files here -->
+</head>
+<body>
+    <header>
+        <!-- Add your header content here -->
+    </header>
+
+    <nav>
+        <!-- Add your navigation menu or links here -->
+    </nav>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <!-- Add your footer content here -->
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentDashboardController;
+use App\Http\Controllers\ProjectApplicationController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\ComplaintController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectPhaseDataController;
+use App\Http\Controllers\ProjectPhaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +23,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [StudentDashboardController::class, 'index'])->name('student.dashboard');
+
+Route::get('/projects/apply', [ProjectApplicationController::class, 'create'])->name('student.project.application.create');
+Route::post('/projects/apply', [ProjectApplicationController::class, 'store'])->name('student.project.application.store');
+
+Route::get('/feedback/status', [ProjectApplicationController::class, 'index'])->name('student.feedback.status');
+
+Route::get('/complaints/create', [ComplaintController::class, 'create'])->name('student.complaint.submit');
+Route::post('/complaints', [ComplaintController::class, 'store'])->name('student.complaints.store');
+
+Route::get('/projects/upload/{project}', [ProjectPhaseController::class, 'create'])->name('student.project.upload');
+Route::post('/projects/upload/{project}', [ProjectPhaseDataController::class, 'store'])->name('student.project.upload.store');
+
+
